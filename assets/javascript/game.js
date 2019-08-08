07818dae02d3b1869c21a7b2def51a2beaafb7e6
William Mooney Sloneker <wmsloneker@gmail.com>
2019-08-08 19:06:44 -0400

    Began defining variables for the elements with randomly assigned numerical values

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+var rubyValue = Math.floor(Math.random() * 12) + 1;
+var sapphireValue = Math.floor(Math.random() * 12) + 1;
+var topazValue = Math.floor(Math.random() * 12) + 1;
+var emeraldValue = Math.floor(Math.random() * 12) + 1;
+
+console.log("Ruby Value: " + rubyValue);
+console.log("Sapphire Value: " + sapphireValue);
+console.log("Topaz Value: " + topazValue);
+console.log("Emerald Value: " + emeraldValue);
+
+var targetScore = Math.floor(Math.random() * 101) + 19;
+
+console.log("Target Score: " + targetScore);
