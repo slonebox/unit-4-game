2df4e7cddf99a060b342de5d3f99c59d197f4194
William Mooney Sloneker <wmsloneker@gmail.com>
2019-08-09 18:06:48 -0400

    Developed if/else criteria to check score and record wins and losses; developed reset game function to generate new gemstone values and target score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 // Scoreboard at the beginning of the game and displays them on the screen
 
 var wins = 0;
@@ -13,10 +14,24 @@ $("#your-score").html(yourScore);
 var oddValues = [1,3,5,7,9,11];
 var evenValues = [2,4,6,8,10,12];
 
-var rubyValue = oddValues[Math.floor(Math.random() * oddValues.length)];
-var sapphireValue = Math.floor(Math.random() * 12) + 1;
-var topazValue = Math.floor(Math.random() * 12) + 1;
-var emeraldValue = evenValues[Math.floor(Math.random() * evenValues.length)];
+//Retrieves random values for the gemstones and the target score
+function getOddValues() {
+    return oddValues[Math.floor(Math.random() * oddValues.length)];
+}
+function getEvenValues() {
+    return evenValues[Math.floor(Math.random() * evenValues.length)];
+}
+function getRandomValues() {
+    return Math.floor(Math.random() * 12) + 1;
+}
+function getTargetScore() {
+    return Math.floor(Math.random() * 101) + 19;
+}
+
+var rubyValue = getOddValues();
+var sapphireValue = getRandomValues();
+var topazValue = getRandomValues();
+var emeraldValue = getEvenValues();
 
 console.log("Ruby Value: " + rubyValue);
 console.log("Sapphire Value: " + sapphireValue);
@@ -24,31 +39,66 @@ console.log("Topaz Value: " + topazValue);
 console.log("Emerald Value: " + emeraldValue);
 
 // Assigns random value to target score between 19 and 120; displays it on the screen
-var targetScore = Math.floor(Math.random() * 101) + 19;
+var targetScore = getTargetScore();
 $("#target-score").html("Target Score: " + targetScore);
 
-$(document).ready(function() {
-    //Adds each gemstone's random value to your score upon each click
-    $("#ruby").on("click", function() {
-        yourScore = yourScore + rubyValue;
-        console.log("Your Score: " + yourScore)
-        $("#your-score").html(yourScore);
-    });
-    $("#sapphire").on("click", function() {
-        yourScore = yourScore + sapphireValue;
-        console.log("Your Score: " + yourScore)
-        $("#your-score").html(yourScore);
-    });
-    $("#topaz").on("click", function() {
-        yourScore = yourScore + topazValue;
-        console.log("Your Score: " + yourScore)
-        $("#your-score").html(yourScore);
-    });
-    $("#emerald").on("click", function() {
-        yourScore = yourScore + emeraldValue;
-        console.log("Your Score: " + yourScore)
-        $("#your-score").html(yourScore);
-    });
-});
+//Resets your score and generates new random values for the next game
+function resetGame() {
+    yourScore = 0;
+    rubyValue = getOddValues();
+    sapphireValue = getRandomValues();
+    topazValue = getRandomValues();
+    emeraldValue = getEvenValues();
+    targetScore = getTargetScore();
+    $("#target-score").html("Target Score: " + targetScore);
+    console.log("Ruby Value: " + rubyValue);
+    console.log("Sapphire Value: " + sapphireValue);
+    console.log("Topaz Value: " + topazValue);
+    console.log("Emerald Value: " + emeraldValue);
+};
 
-// 
+// Checks if game should continue based on your score vs. the target score
+function checkScore() {
+    if (yourScore === targetScore) {
+        alert("Congratulations! You won!");
+        wins++;
+        $("#wins").html(wins);
+        $("#wins").html("Wins: " + wins);
+        resetGame();
+    } else if (yourScore > targetScore) {
+        alert("You lose! Dummy!");
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        resetGame();
+    };
+};
+
+//Adds each gemstone's random value to your score upon each click
+$(document).ready(function() {
+
+        $("#ruby").on("click", function() {
+            yourScore = yourScore + rubyValue;
+            console.log("Your Score: " + yourScore)
+            $("#your-score").html(yourScore);
+            checkScore();
+        });
+        $("#sapphire").on("click", function() {
+            yourScore = yourScore + sapphireValue;
+            console.log("Your Score: " + yourScore)
+            $("#your-score").html(yourScore);
+            checkScore();
+        });
+        $("#topaz").on("click", function() {
+            yourScore = yourScore + topazValue;
+            console.log("Your Score: " + yourScore)
+            $("#your-score").html(yourScore);
+            checkScore();
+        });
+        $("#emerald").on("click", function() {
+            yourScore = yourScore + emeraldValue;
+            console.log("Your Score: " + yourScore)
+            $("#your-score").html(yourScore);
+            checkScore();
+        });
+
+});
